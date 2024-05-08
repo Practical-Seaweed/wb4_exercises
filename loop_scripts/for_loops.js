a42935918619eefe6bcdb28766436bf701c6fa71
Wilfredo Contreras <wilfredojrcontreras@gmail.com>
2024-05-08 14:40:39 -0500

    for_loop and while_loops exercise

---
+++ loop_scripts/for_loops.js
@@ -0,0 +1,6 @@
+"use strict"
+
+// use a for loop to print out the phrase "I love loops" 7 times.
+for (let i = 0; i < 7; i++) {
+    console.log("I love loops");
+}
---
+++ loop_scripts/while_loops.js
@@ -0,0 +1,8 @@
+"use strict"
+
+// use a while loop to print out the phrase "I love loops" 7 times.
+let i = 0;
+while (i < 7) {
+    console.log("I love loops");
+    i++;
+}
