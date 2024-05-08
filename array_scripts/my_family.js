b12eaf6454af8d894d2c0cfff6b696bec1c32c3d
Wilfredo Contreras <wilfredojrcontreras@gmail.com>
2024-05-08 15:46:09 -0500

    did my_family exercise

---
+++ array_scripts/my_family.js
@@ -0,0 +1,15 @@
+"use strict"
+
+// created and Array (list) of family member names
+let myFamilyArray = ["will", "father", "mother", "edward"];
+
+// get the number of items on the list (in this specific case it should be 4)
+let numberOfFamilyMembers = myFamilyArray.length;
+
+// loop Over my family array and print the names
+for(let i=0; i < numberOfFamilyMembers; i++){
+
+    // pring out each family members name
+    console.log(myFamilyArray[i]);
+
+}
