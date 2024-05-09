8ee6e9af099e56215a0d7cf00ad951f7c2ba3681
Wilfredo Contreras <wilfredojrcontreras@gmail.com>
2024-05-09 13:08:09 -0500

    following along eric demo

---
+++ eric_demos/addingToArrays.js
@@ -0,0 +1,35 @@
+"use strict"
+
+let namesArray = ["eric", "alec", "italy", "candice", "rohan"];
+
+// namesArray[namesArray.length] = "katalina";
+// namesArray[namesArray.length] = "adriana";
+// namesArray[namesArray.length] = "wilfredo";
+
+
+// namesArray.push("katalina", "adriana", "wilfredo") this also works instead of doing it on a new line
+namesArray.push("katalina");
+namesArray.push("adriana");
+namesArray.push("wilfredo");
+namesArray.push("cash", "is", "kings");
+
+console.log(namesArray);
+
+// removes the last item
+namesArray.pop();
+
+// removes the first item
+namesArray.shift();
+
+console.log(namesArray);
+
+/*
+⣴⡿⠶⠀⠀⠀⣦⣀⣴⠀⠀⠀⠀
+⣿⡄⠀⠀⣠⣾⠛⣿⠛⣷⠀⠿⣦ 
+⠙⣷⣦⣾⣿⣿⣿⣿⣿⠟⠀⣴⣿
+⠀⣸⣿⣿⣿⣿⣿⣿⣿⣾⠿⠋⠁
+⠀⣿⣿⣿⠿⡿⣿⣿⡿⠀⠀⠀⠀
+⢸⣿⡋⠀⠀⠀⢹⣿⡇⠀⠀⠀⠀
+⣿⡟⠀⠀⠀⠀⠀⢿⡇⠀⠀⠀⠀
+⠉⠁⠀⠀⠀⠀⠀⠸⠇⠀⠀⠀⠀
+*/
